36d72c82cd96a6c26a238e288439bc6d5257a6d0
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-13 23:36:09 -0600

    Materials and Positioning

--- Raytracer/Raytracer/Raytracer.py
+++ Raytracer/Raytracer/Raytracer.py
@@ -1,3 +1,4 @@
+from token import OP
 import pygame
 from pygame.locals import *
 from rt import Raytracer
@@ -5,7 +6,7 @@ from figures import *
 from lights import *
 from materials import *
 
-width = 100
+width = 256
 height = 100
 
 pygame.init()
@@ -17,67 +18,62 @@ screen.set_alpha(None)
 # Instanciar el raytracer
 raytracer = Raytracer(screen)
 
-raytracer.environmentMap = pygame.image.load("maps/parking_lot_map.jpg")
+raytracer.environmentMap = pygame.image.load("maps/future_map.jpg")
 
 raytracer.rtClearColor(0.25, 0.25, 0.25)
 
-earthTexture = pygame.image.load("textures/earth_tex.jpg")
-marbleTexture = pygame.image.load("textures/marble_tex.jpg")
-boxTexture = pygame.image.load("textures/wood_box.jpg")
+# ---------------------------------------- Creacion de Texturas ----------------------------------------
+
+iridTexture   = pygame.image.load("textures/iridescent_tex.jpg")
+scalyTexture  = pygame.image.load("textures/scale_tex.jpg")
+spralTexture  = pygame.image.load("textures/spratlite_tex.png")
+metalTexture  = pygame.image.load("textures/metalex.jpg")
 
 # --------------------------------------- Creacion de materiales ---------------------------------------
 # Opacos
-brick = Material(diffuse=(1, 0.4, 0.4), spec = 8, Ks = 0.01)
-grass = Material(diffuse=(0.4, 1, 0.4), spec = 32, Ks = 0.1)
-water = Material(diffuse=(0.4, 0.4, 1), spec = 256, Ks = 0.2)
-# Reflectivos
-
-# Transparentes
-
-# con texturas
-earth = Material(texture = earthTexture)
-marble = Material(texture= marbleTexture)
-box = Material(spec = 8, Ks = 0.05, matType = OPAQUE, texture = boxTexture)
+scaly = Material(spec = 64, Ks = 0.2, texture = scalyTexture)
+metalex = Material(spec = 32, Ks = 0.1, texture = metalTexture)
 
 # Reflectivos
-mirror = Material(diffuse=(0.9, 0.9, 0.9), spec = 64, Ks = 0.2, matType = REFLECTIVE)
-blueMirror = Material(diffuse=(0.4, 0.4, 0.9), spec = 32, Ks = 0.15, matType = REFLECTIVE)
+spratlite = Material(spec = 64, Ks = 0.2, matType = REFLECTIVE, texture = spralTexture)
 
 # Transparentes
-glass = Material(diffuse=(0.9, 0.9, 0.9), spec = 64, Ks = 0.15, ior= 1.5, matType = TRANSPARENT)
-diamond = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 1.5, matType = TRANSPARENT)
+iridescent = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 1.5, matType = TRANSPARENT, texture = iridTexture)
 
 # ---------------------------------------- Figuras en la escena ----------------------------------------
 # Triangulos
 
-# Triangulo Transparente
+# Triangulo Opaco
 raytracer.scene.append(Triangle(
-    v0=[-1, -1, -2],
-    v1=[ 1, -1, -2],
+    v0=[-1.2, -1, -2],
+    v1=[ 1.2, -1, -2],
     v2=[ 0,  1, -2],
-    material=brick
+    material = metalex
 ))
 
 # Triangulo Reflectivo
 raytracer.scene.append(Triangle(
-    v0=[-4, -3, -5],
-    v1=[ 1,  1, -7],
-    v2=[-3,  3, -8],
-    material = brick 
+    v0=[-5, 2, -4],
+    v1=[ 1,  1, -11],
+    v2=[-4,  -2.3, -6],
+    material = spratlite 
 ))
 
-# Triangulo Opaco
+# Triangulo Transparente
 raytracer.scene.append(Triangle(
-    v0=[0, -2, -4],
-    v1=[3,  -1, -5],
-    v2=[2,  2, -5],
-    material = brick 
+    v0=[0, -2, -6],
+    v1=[6,  -1, -5],
+    v2=[3,  2, -5],
+    material = iridescent
 ))
+
+# Extras
+raytracer.scene.append(Sphere(position = [0,0,-4], radius = 1, material = scaly))
 
 # ----------------------------------------- Luces de la escena -----------------------------------------
 raytracer.lights.append(AmbientLight(intensity=0.1))
 raytracer.lights.append(DirectionalLight(direction = (-1,-1,-1), intensity = 0.9))
-raytracer.lights.append(PointLight(point = (2.5,0,-5), intensity = 1, color = (0,1,1)))
+raytracer.lights.append(PointLight(point = (2.5,0,-4.5), intensity = 100, color = (1,0.2,1)))
 
 
 raytracer.rtClear()
