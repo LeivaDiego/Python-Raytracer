500c0d4fb4dd38f89cfb5738535d3f59d5064af0
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-22 06:16:38 -0600

    v7.1 Material Creation

--- Raytracer/Raytracer/Raytracer.py
+++ Raytracer/Raytracer/Raytracer.py
@@ -24,45 +24,51 @@ def main():
 
 	# ---------------------------------------- Creacion de Texturas ----------------------------------------
 
-	#iridTexture   = pygame.image.load("textures/iridescent_tex.jpg")
-	#scalyTexture  = pygame.image.load("textures/scale_tex.jpg")
-	#spralTexture  = pygame.image.load("textures/spratlite_tex.png")
-	#metalTexture  = pygame.image.load("textures/metalex.jpg")
-
-	#tridTexture = pygame.image.load("textures/metalex.jpg")
+	iridTexture	= pygame.image.load("textures/iridescent_tex.jpg")
+	planet1Texture = pygame.image.load("textures/planet1.jpg")
+	planet2Texture = pygame.image.load("textures/planet2.jpg")
+	hexTexture	= pygame.image.load("textures/hex.png")
+	floorTexture	= pygame.image.load("textures/floor.jpg")
+	crystalTexture	= pygame.image.load("textures/crystal.jpg")
+	scalyTexture  = pygame.image.load("textures/scale_tex.jpg")
+	energyTexture  = pygame.image.load("textures/energy.jpg")
 
 	# --------------------------------------- Creacion de materiales ---------------------------------------
-	green = Material(diffuse=(0, 1, 0), spec = 32, Ks = 0.1)
-	yellow = Material(diffuse=(1, 1, 0), spec = 64, Ks = 0.2)
-	red = Material(diffuse=(1, 0, 0), spec = 32, Ks = 0.1)
-	blue = Material(diffuse=(0, 0, 1), spec = 32, Ks = 0.1)
-	xd = Material(diffuse=(0,1,1), spec = 32, Ks = 0.1, matType=REFLECTIVE)
+
+	
 
 
+	# Opacos
+	scaly = Material(spec = 64, Ks = 0.2, texture = scalyTexture)
+	planetHex = Material(spec = 32, Ks = 0.1, texture = hexTexture)
+	planet1 = Material(spec = 32, Ks = 0.15, texture = planet1Texture)
+	planet2 = Material(spec = 32, Ks = 0.15, texture = planet2Texture)
+	gold = Material(diffuse=(0.9, 1, 0), spec = 64, Ks = 0.2)
 
-	## Opacos
-	#scaly = Material(spec = 64, Ks = 0.2, texture = scalyTexture)
-	#metalex = Material(spec = 32, Ks = 0.1, texture = metalTexture)
+	# Reflectivos
+	floor = Material(spec = 64, Ks = 0.2, matType = REFLECTIVE, texture = floorTexture)
+	portal = Material(diffuse = (0.6, 0.2, 1), spec = 128, Ks = 0.25, matType = REFLECTIVE)
+	hexSphere = Material(diffuse = (0.6, 0.2, 1), spec = 128, Ks = 0.25, matType = REFLECTIVE, texture = hexTexture)
+	podium = Material(spec = 128, Ks = 0.25, matType = REFLECTIVE, texture = energyTexture)
 
-	## Reflectivos
-	#spratlite = Material(spec = 64, Ks = 0.2, matType = REFLECTIVE, texture = spralTexture)
-
-	## Transparentes
-	#iridescent = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 1.5, matType = TRANSPARENT, texture = iridTexture)
+	# Transparentes
+	iridescent = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 1.5, matType = TRANSPARENT, texture = iridTexture)
+	crystal = Material(diffuse=(0.6, 1, 8), spec = 130, Ks = 0.25, ior= 2.0, matType = TRANSPARENT, texture = crystalTexture)
+	ghostPlanet = Material(spec = 130, Ks = 0.25, ior= 2.0, matType = TRANSPARENT, texture = planet1Texture)
 
 	# ---------------------------------------- Figuras en la escena ----------------------------------------
 
 
-	raytracer.scene.append(AABB(position = (0,  -5, -6), size = (8,8,8), material = green))
-	raytracer.scene.append(AABB(position = (0,  1, -14), size = (8,8,8), material = yellow))
+	raytracer.scene.append(AABB(position = (0,  -5, -6), size = (8,8,8), material = floor))
+	raytracer.scene.append(AABB(position = (0,  1, -14), size = (8,8,8), material = crystal))
 
-	raytracer.scene.append(Sphere(position = [-5,2,-10], radius = 1, material = red))
-	raytracer.scene.append(Sphere(position = [-5,1.5,-7.5], radius = 1, material = blue))
-	raytracer.scene.append(Sphere(position = [-5,1,-5], radius = 1, material = xd))
+	raytracer.scene.append(Sphere(position = [-5,2,-10], radius = 1, material = scaly))
+	raytracer.scene.append(Sphere(position = [-5,1.5,-7.5], radius = 1, material = hexSphere))
+	raytracer.scene.append(Sphere(position = [-5,1,-5], radius = 1, material = planet1))
 
-	raytracer.scene.append(Sphere(position = [5,2,-10], radius = 1, material = red))
-	raytracer.scene.append(Sphere(position = [5,1.5,-7.5], radius = 1, material = blue))
-	raytracer.scene.append(Sphere(position = [5,1,-5], radius = 1, material = xd))
+	raytracer.scene.append(Sphere(position = [5,2,-10], radius = 1, material = planetHex))
+	raytracer.scene.append(Sphere(position = [5,1.5,-7.5], radius = 1, material = planet2))
+	raytracer.scene.append(Sphere(position = [5,1,-5], radius = 1, material = ghostPlanet))
 
 	#trident = Model(file = 'models/trident.obj', 
 	#							translate = (0,0.3,-5),
@@ -77,21 +83,20 @@ def main():
 	#							material = blue))
 
 
-	
-	raytracer.scene.append(AABB(position = (0, -1, -4), size = (1,1,1), material = yellow))
+	raytracer.scene.append(AABB(position = (0, -1, -4), size = (1,1,1), material = gold))
 
-	raytracer.scene.append(Disk(position=(0,2,-9.5), normal=(0,0,1), radius = 2, material= xd))
+	raytracer.scene.append(Disk(position=(0,2,-9.5), normal=(0,0,1), radius = 2, material= portal))
 
-	#raytracer.scene.append(Triangle(v0=[-1.2, -1, -2],
-	#							 v1=[ 1.2, -1, -2],
-	#							 v2=[ 0,  1, -2],
-	#							 material = xd))
+	raytracer.scene.append(Triangle(v0=[-1.2, -1, -2],
+								 v1=[ 1.2, -1, -2],
+								 v2=[ 0,  1, -2],
+								 material = iridescent))
 
 	# ----------------------------------------- Luces de la escena -----------------------------------------
 	raytracer.lights.append(AmbientLight(intensity = 0.1))
 	raytracer.lights.append(DirectionalLight(direction = (-1,-1,-1), intensity = 0.5))
 	#raytracer.lights.append(DirectionalLight(direction = (1,-1,-1), intensity = 0.5))
-	raytracer.lights.append(PointLight(point = (0,2,-4.5), intensity = 25, color = (1,0.2,0)))
+	#raytracer.lights.append(PointLight(point = (0,2,-4.5), intensity = 25, color = (1,0.2,0)))
 
 	raytracer.rtClear()
 	raytracer.rtRender()
