028bf4256b14236beaf7bef6145cf1ce450e04da
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-27 01:05:19 -0600

    New Scene V2
    - Nueva escena, con mejor representacion de la imagen de referencia.
    - Mejor perspectiva, texturas mas acorde, materiales mas acorde, menos conglomerado.
    tiempo de renderizacion 309 segundos

--- Raytracer/Raytracer/Raytracer.py
+++ Raytracer/Raytracer/Raytracer.py
@@ -1,3 +1,4 @@
+from turtle import color
 import pygame
 from pygame.locals import *
 from rt import Raytracer
@@ -6,8 +7,8 @@ from lights import *
 from materials import *
 
 def main():
-	width = 768
-	height = 432
+	width = 100
+	height = 100
 
 	pygame.init()
 
@@ -18,75 +19,104 @@ def main():
 	# Instanciar el raytracer
 	raytracer = Raytracer(screen)
 
-	raytracer.environmentMap = pygame.image.load("maps/nebula.jpg")
+	raytracer.environmentMap = pygame.image.load("maps/space.jpg")
 
 	raytracer.rtClearColor(0.25, 0.25, 0.25)
 
 	# ---------------------------------------- Creacion de Texturas ----------------------------------------
 
-	iridTexture	= pygame.image.load("textures/iridescent_tex.jpg")
+	
 	planet1Texture = pygame.image.load("textures/planet1.jpg")
 	planet2Texture = pygame.image.load("textures/planet2.jpg")
 	planet3Texture = pygame.image.load("textures/planet3.jpg")
-	planet4Texture  = pygame.image.load("textures/scale_tex.jpg")
-	hexTexture	= pygame.image.load("textures/hex.png")
-	floorTexture	= pygame.image.load("textures/floor.jpg")
-	crystalTexture	= pygame.image.load("textures/crystal.jpg")
-	energyTexture  = pygame.image.load("textures/energy.jpg")
-	metalexTexture = pygame.image.load("textures/metalex.jpg")
+	planet4Texture = pygame.image.load("textures/planet4.jpg")
+
+	floorTexture = pygame.image.load("textures/floor.jpg")
+	titaTexture = pygame.image.load("textures/titanium.jpg")
+
+	energyTexture  = pygame.image.load("textures/energy.jpeg")
+	iridTexture	= pygame.image.load("textures/iridescent.jpg")
 
 	# --------------------------------------- Creacion de materiales ---------------------------------------
 
 	# Opacos
-	scalyPlanet = Material(spec = 64, Ks = 0.2, texture = planet4Texture)
-	planet1 = Material(spec = 32, Ks = 0.15, texture = planet1Texture)
-	planet2 = Material(spec = 32, Ks = 0.15, texture = planet2Texture)
-	metallicPlanet = Material(spec = 128, Ks = 0.25, texture = metalexTexture)
-	podium = Material(diffuse=(1, 0.7, 0.2),spec = 128, Ks = 0.25, texture = energyTexture)
-	gold = Material(diffuse=(0.7, 1, 0), spec = 64, Ks = 0.2)
+	planet1 = Material(spec = 2,  Ks = 0.04, texture = planet1Texture)
+	planet2 = Material(spec = 2,  Ks = 0.04, texture = planet2Texture)
+	planet3 = Material(spec = 2,  Ks = 0.04, texture = planet3Texture)
+	planet4 = Material(spec = 2,  Ks = 0.04, texture = planet4Texture)
+
+	pilar = Material(spec = 64, Ks = 0.25, texture = titaTexture)
+	gold = Material(diffuse=(1, 1, 1), spec = 10, Ks = 0.01)
 
 	# Reflectivos
-	floor = Material(spec = 64, Ks = 0.2, matType = REFLECTIVE, texture = floorTexture)
-	portal = Material(diffuse = (0.6, 0.2, 1), spec = 128, Ks = 0.25, matType = REFLECTIVE)
-	planetHex = Material(spec = 32, Ks = 0.1, matType = REFLECTIVE, texture = hexTexture)
+	floor = Material(spec = 256,  Ks = 0.6, matType = REFLECTIVE, texture = floorTexture)
+	base  = Material(diffuse = (0.2,0.8,0.8), spec = 256, Ks = 0.6, matType = REFLECTIVE)
 
 	# Transparentes
-	iridescent = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 0.8, matType = TRANSPARENT, texture = iridTexture)
-	crystal = Material(diffuse=(0.6, 1, 0.8), spec = 130, Ks = 0.25, ior= 0.5, matType = TRANSPARENT, texture = crystalTexture)
-	planet3 = Material(spec = 130, Ks = 0.25, ior= 1.0, matType = TRANSPARENT, texture = planet3Texture)
+	portal = Material(spec = 100, Ks = 0.8, ior = 2.5, matType = TRANSPARENT, texture = energyTexture)
+	iridescent = Material(diffuse=(0.9, 0.9, 0.9), spec = 128, Ks = 0.2, ior= 0.8, matType = TRANSPARENT, texture = iridTexture) 
 
 	# ---------------------------------------- Figuras en la escena ----------------------------------------
+	# Suelo
+	raytracer.scene.append(AABB(position = (0,  -1.5, -6.5), size = (13,0.1,10), material = floor))
 
-	raytracer.scene.append(AABB(position = (0,  -5, -6), size = (8,8,8), material = floor))
-	raytracer.scene.append(AABB(position = (0,  1, -14), size = (8,8,8), material = crystal))
+	## Base circular
+	raytracer.scene.append(Disk(position=(0,-1.4,-5), normal=(0,1,0), radius = 2, material = base))
+	raytracer.scene.append(Disk(position=(0,-1.35,-5), normal=(0,1,0), radius = 1.75, material = base))
+	raytracer.scene.append(Disk(position=(0,-1.3,-5), normal=(0,1,0), radius = 1.5, material = base))
+	raytracer.scene.append(Disk(position=(0,-1.25,-5), normal=(0,1,0), radius = 1.25, material = base))
+	raytracer.scene.append(Disk(position=(0,-1.2,-5), normal=(0,1,0), radius = 1, material = base))
 
-	raytracer.scene.append(Sphere(position = [-5,2,-10], radius = 1, material = scalyPlanet))
-	raytracer.scene.append(Sphere(position = [-5,1.5,-7.5], radius = 1, material = planet3))
-	raytracer.scene.append(Sphere(position = [-5,1,-5], radius = 1, material = planet1))
+	# Portal
+	raytracer.scene.append(AABB(position = (0,  2, -11), size = (4,4,4), material = portal))
 
-	raytracer.scene.append(Sphere(position = [5,2,-10], radius = 1, material = planet2))
-	raytracer.scene.append(Sphere(position = [5,1.5,-7.5], radius = 1, material = planetHex))
-	raytracer.scene.append(Sphere(position = [5,1,-5], radius = 1, material = metallicPlanet))
+	# Planetas
+	raytracer.scene.append(Sphere(position = [7,1,-18], radius = 2.5, material = planet2))
+	raytracer.scene.append(Sphere(position = [10,9,-22], radius = 2, material = planet3))
+	raytracer.scene.append(Sphere(position = [10,8,-30], radius = 1, material = planet1))
+	raytracer.scene.append(Sphere(position = [-8,8,-35], radius = 10, material = planet4))
 
+	# Pilares
+	raytracer.scene.append(AABB(position = (-2.5,-1,  -8), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (-3,  -1,  -7), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (-5,  -1,  -9), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (-2.5,-1, -10), size = (0.2,1,0.2), material = pilar))
+
+	raytracer.scene.append(AABB(position = (2.5,-1,  -8), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (3,  -1,  -7), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (5,  -1,  -9), size = (0.2,1,0.2), material = pilar))
+	raytracer.scene.append(AABB(position = (2.5,-1, -10), size = (0.2,1,0.2), material = pilar))
+
+	# Tridente
 	raytracer.scene.append(Model(file = 'models/trident.obj', 
-								translate = (0,0.3,-4),
-								rotate = (-75,45,10),
+								translate = (0, -0.2, -5),
+								rotate = (270, 0, 0),
+								scale = (0.4, 0.01, 0.5),
 								material = gold))
 
-	raytracer.scene.append(AABB(position = (0, -1, -4), size = (1,1,1), material = podium))
-
-	raytracer.scene.append(Disk(position=(0,2,-9.5), normal=(0,0,1), radius = 2, material= portal))
-
-	raytracer.scene.append(Triangle(v0=[-1.2, -1, -2],
-								 v1=[ 1.2, -1, -2],
-								 v2=[ 0,  1, -2],
+	raytracer.scene.append(Triangle(v0=[-0.3, -1, -2],
+								 v1=[ 0.3, -1, -2],
+								 v2=[ 0,  -0.5, -2.1],
 								 material = iridescent))
+
+	raytracer.scene.append(Triangle(v0=[0.5, -1, -2.1],
+								 v1=[ 1.1, -1, -2.6],
+								 v2=[ 0.8,  -0.4, -2.5],
+								 material = iridescent))
+
+	raytracer.scene.append(Triangle(v0=[-0.5, -1, -2.1],
+								v1=[ -1.1, -1, -2.6],
+								v2=[ -0.8,  -0.4, -2.5],
+								material = iridescent))
+
 
 	# ----------------------------------------- Luces de la escena -----------------------------------------
 	raytracer.lights.append(AmbientLight(intensity = 0.1))
-	raytracer.lights.append(DirectionalLight(direction = (-1,-1,-1), intensity = 0.5))
-	raytracer.lights.append(DirectionalLight(direction = (1,-1,-1), intensity = 0.5))
-	raytracer.lights.append(PointLight(point = (0,2,-4.5), intensity = 5, color = (0.5,0.2,0.9)))
+	raytracer.lights.append(DirectionalLight(direction = (0,-1,-1), intensity = 0.5))
+	raytracer.lights.append(PointLight(point = (0,-0.85,-5.15), intensity = 10, color = (1,1,1)))
+	raytracer.lights.append(PointLight(point = (-20,5,-20), intensity = 50, color = (1,0.3,0)))
+	raytracer.lights.append(PointLight(point = (15,6,-15), intensity = 20, color = (0,0.7,1)))
+	raytracer.lights.append(PointLight(point = (0,2.45,-8), intensity = 5, color = (0,1,1)))
 
 	raytracer.rtClear()
 	raytracer.rtRender()
